62535b4b29321f69876b302c71644fccdd6168f6
Ardhi Lukianto <ardhi@lukianto.com>
2023-09-05 20:15:22 +0700

    Refactor

--- bajo/helper/route-path.js
+++ bajo/helper/route-path.js
@@ -1,12 +1,11 @@
-function routePath (name, { query = {} } = {}, base = 'bajoWebMpa') {
+function routePath (name, { query = {}, base = 'bajoWebMpa', params = {} } = {}) {
   const { getConfig } = this.bajo.helper
   const { routeDir, defaultsDeep } = this.bajoWeb.helper
   const { breakNsPath } = this.bajo.helper
   const cfg = getConfig(base)
   const [ns, fullPath] = breakNsPath(name)
   let [path, qs] = fullPath.split('?')
-  if (cfg.i18nDetectors.includes('path')) path = `/:lang${path}`
-  let url = routeDir(ns) + path
+  let url = cfg.i18nDetectors.includes('path') ? `/${params.lang ?? ''}${routeDir(ns)}${path}` : `${routeDir(ns)}${path}`
   if (this.bajoWeb.vars) {
     const { queryString, isEmpty } = this.bajoWeb.vars
     qs = defaultsDeep(query, queryString.parse(qs))
--- bajo/start.js
+++ bajo/start.js
@@ -1,16 +1,18 @@
 import fastify from 'fastify'
 import appHook from '../lib/app-hook.js'
 import routeHook from '../lib/route-hook.js'
-import bootApp from '../lib/boot-app.js'
+import printRoutes from '../lib/print-routes.js'
+import { boot } from '../lib/app.js'
 import fastifySensible from '@fastify/sensible'
+import fastifyNoicon from 'fastify-no-icon'
 
 async function start () {
   const { importPkg, getConfig, generateId, runHook, error } = this.bajo.helper
   const { cloneDeep } = await importPkg('lodash-es')
   const queryString = await importPkg('bajo-extra:query-string')
-  const opts = getConfig('bajoWeb')
-  const optsFactory = cloneDeep(opts.factory)
-  const optsServer = cloneDeep(opts.server)
+  const cfg = getConfig('bajoWeb')
+  const optsFactory = cloneDeep(cfg.factory)
+  const optsServer = cloneDeep(cfg.server)
   optsFactory.logger = this.bajoLogger.instance.child(
     {},
     { msgPrefix: '[bajoWeb] ' }
@@ -28,10 +30,12 @@ async function start () {
   this.bajoWeb.instance = instance
   await runHook('bajoWeb:afterCreateContext', instance)
   await instance.register(fastifySensible)
+  await instance.register(fastifyNoicon)
   await appHook.call(this)
   await routeHook.call(this)
-  await bootApp.call(this)
+  await boot.call(this)
   await runHook('bajoWeb:afterBootApp')
+  if (cfg.printRoutes) await printRoutes.call(this)
   await instance.listen(cloneDeep(optsServer))
 }
 
