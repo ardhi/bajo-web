d3f1509b5be1f800da2d692ac42b985d4ac11a66
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-11 21:15:43 +0700

    Bug fix

--- bajo/helper/record/schema.js
+++ bajo/helper/record/schema.js
@@ -1,8 +1,9 @@
 async function schema ({ repo, req }) {
-  const { getPlugin } = this.bajo.helper
+  const { getPlugin, importPkg } = this.bajo.helper
   const { getSchema } = this.bajoDb.helper
+  const { pick } = await importPkg('lodash-es')
   getPlugin('bajoDb') // ensure bajoDb is loaded
-  const schema = await getSchema(repo ?? req.params.repo)
+  const schema = pick(await getSchema(repo ?? req.params.repo), ['name', 'properties', 'disabled'])
   return schema
 }
 
