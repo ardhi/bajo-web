1e7046a56f3127845929ec74556db61c4f63f3fd
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-13 20:48:15 +0700

    Safe schema

--- bajo/helper/record/schema.js
+++ bajo/helper/record/schema.js
@@ -1,9 +1,11 @@
-async function schema ({ coll, req }) {
+async function schema (coll, meta = {}) {
   const { getPlugin, importPkg } = this.bajo.helper
   const { getSchema } = this.bajoDb.helper
-  const { pick } = await importPkg('lodash-es')
+  const { pick, get, filter } = await importPkg('lodash-es')
   getPlugin('bajoDb') // ensure bajoDb is loaded
-  const schema = pick(await getSchema(coll ?? req.params.coll), ['name', 'properties', 'disabled'])
+  const schema = pick(await getSchema(coll), ['name', 'properties', 'disabled'])
+  const hidden = get(meta, 'display.list.hidden', [])
+  schema.properties = filter(schema.properties, p => !hidden.includes(p.name))
   return schema
 }
 
