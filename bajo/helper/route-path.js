a2d799f237a28d52a3f1026499c7c8df87316948
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-16 20:42:29 +0700

    Bug fix

--- bajo/helper/route-path.js
+++ bajo/helper/route-path.js
@@ -1,16 +1,17 @@
 function routePath (name, { query = {}, base = 'bajoWebMpa', params = {} } = {}) {
-  const { getConfig } = this.bajo.helper
-  const { routeDir, defaultsDeep } = this.bajoWeb.helper
+  const { getConfig, defaultsDeep } = this.bajo.helper
+  const { routeDir } = this.bajoWeb.helper
   const { breakNsPath } = this.bajo.helper
   const cfg = getConfig(base)
   const [ns, fullPath] = breakNsPath(name)
-  let [path, qs] = fullPath.split('?')
+  let [path, queryString] = fullPath.split('?')
+  path = path.split('/').map(p => {
+    return p[0] === ':' && params[p.slice(1)] ? params[p.slice(1)] : p
+  }).join('/')
   let url = cfg.i18n.detectors.includes('path') ? `/${params.lang ?? ''}${routeDir(ns)}${path}` : `${routeDir(ns)}${path}`
-  if (this.bajoWeb.vars) {
-    const { queryString, isEmpty } = this.bajoWeb.vars
-    qs = defaultsDeep(query, queryString.parse(qs))
-    if (!isEmpty(qs)) url += '?' + queryString.stringify(qs)
-  }
+  const { qs, isEmpty } = this.bajoWebMpa.util
+  queryString = defaultsDeep(query, qs.parse(queryString))
+  if (!isEmpty(queryString)) url += '?' + qs.stringify(queryString)
   return url
 }
 
