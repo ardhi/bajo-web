1584aa7ac2a006e1830d030adb93fcc52a89e161
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-24 19:43:56 +0700

    Updates

--- bajo/hook/on-request.js
+++ bajo/hook/on-request.js
@@ -2,11 +2,14 @@ const onRequest = {
   level: 5,
   handler: async function onRequest (ctx, req, reply) {
     const { log, getConfig } = this.bajo.helper
+    const { getIp } = this.bajoWeb.helper
     const cfg = getConfig('bajoWeb')
     req.site = cfg.siteInfo
     let msg = '< %s:%s from IP %s'
     if (req.headers['content-length']) msg += ', content length: %s'
-    log.info(msg, req.method, req.url, req.ip, req.headers['content-length'])
+    let fwd = req.headers['x-forwarded-for'] ?? ''
+    if (!Array.isArray(fwd)) fwd = fwd.split(',').map(ip => ip.trim())
+    log.info(msg, req.method, req.url, getIp(req), req.headers['content-length'])
   }
 }
 
